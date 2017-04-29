ca266bdf8337608a408b6a9e4fee052780542301
Shivan <shivan.nawal@gmail.com>
2017-04-30 03:25:12 +0530

    view ready -shivan

--- pilotsplus/views.py
+++ pilotsplus/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import sslresolved
+from lxml import html
+import requests
 
 # Create your views here.
+def scrape_data(request):
+	query = request.GET.get('q', None)
+	age = requests.get('https://www.britannica.com/place/Argentina')
+	tree = html.fromstring(page.content)
+
+	intro = tree.xpath('//*[@id="toc33101"]/p/text()')
+
+	data = ''.join(intro)
+
+	html = "<html><body>%s </body></html>" % data
+	return HttpResponse(html)
